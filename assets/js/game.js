40bdceece82a78f563825dd9748672059adbe13a
doker123 <dodo2larin@gmail.com>
2025-10-06 09:48:02 +0700

    game.js main.js

---
+++ assets/js/game.js
@@ -0,0 +1,25 @@
+class Game {
+    constructor() {
+        this.name = name;
+    }
+
+    start () {
+        this.loop();
+    }
+
+    loop() {
+        requestAnimationFrame(() => {
+            this.setParams();
+            this.loop();
+        });
+    }
+
+    setParams() {
+        let params = ['name'];
+        let values = [this.name];
+        params.forEach((param, ind ) =>{
+            $(`#${param}`).innerHTML = values[ind];
+        });
+    }
+
+}
